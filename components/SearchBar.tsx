8e7078ffd7be4fa7beba990c0b244dc05c8dc9dd
Anadudev <anadudev@gmail.com>
2025-05-05 05:21:24 +0100

    feature: create search bar component

---
+++ components/SearchBar.tsx
@@ -0,0 +1,26 @@
+import { View, Image, TextInput } from "react-native";
+import React from "react";
+import { icons } from "@/constants/icons";
+
+const SearchBar = ({ onPress, placeholder }) => {
+  return (
+    <View className="flex-row items-center bg-dark-500 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor={"#ab8bff"}
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor={"#a8b5db"}
+        className="flex- ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
